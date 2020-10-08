27c1e1be0635a9677475da29ada9d87ac2674671
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-10-08 21:33:51 +0100

    ✨ Introduce Howler

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import {Howl, Howler} from 'howler';
+
+const disc = document.querySelector(".disc")
+const song = new Howl({
+    src: ['../assets/audio/plastic-brains.mp3']
+})
+
+const moveDisc = () => {
+    disc.classList.toggle("translate")
+} 
+
+const play = () => song.play()
+
+disc.addEventListener("click", moveDisc)
+disc.addEventListener("click", play)
