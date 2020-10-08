3cebf0a70003fe0ac44eba0e2e9440babd4414c5
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-10-08 21:47:53 +0100

    🐛 📝

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  rules: {
-    indent: ['error', 2]
-  }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4]
+    }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 import {Howl, Howler} from 'howler';
 
-const disc = document.querySelector(".disc")
+const disc = document.querySelector('.disc')
 const song = new Howl({
     src: ['../assets/audio/plastic-brains.mp3']
 })
 
 const moveDisc = () => {
-    disc.classList.toggle("translate")
-} 
+    disc.classList.toggle('translate')
+}
 
 const play = () => song.play()
 
-disc.addEventListener("click", moveDisc)
-disc.addEventListener("click", play)
+disc.addEventListener('click', moveDisc)
+disc.addEventListener('click', play)
